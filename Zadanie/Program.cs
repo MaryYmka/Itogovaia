a1a615fef50fcb9e586f5c9c1b444be7d524af79
Maria <skorynina.m@yandex.ru>
2024-02-02 11:02:46 +0300

    Добавили вывод массива

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -3,10 +3,46 @@
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 //Примеры: [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”] [“1234”, “1567”, “-2”, “computer science”] → [“-2”] [“Russia”, “Denmark”, “Kazan”] → []
 
+string str = GetString();
+int mass = CountStr(str);
 
 string GetString()
 {
-    Console.WriteLine("Введите строку: "); //Вводим строку с консоли
+    Console.WriteLine("Введите строку: "); //Метод для принятия строки с консоли
     string str = Console.ReadLine();
     return str;
+}
+
+void PrintArray(int[] num) //Вывод массива
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.WriteLine(num[i]+ " ");
+    }
+}
+
+int CountStr(string str) //производим подсчет количества элементов в массиве, длина которых меньше либо равна 3 символа;
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+               if (str.Length <=3)
+        {
+            count++;
+        }
+    }
+    return count;
+    }
+string[] NewArray = new string[str.Length]; //создаем новый массив размером, равным количеству подсчитанных элементов;
+{
+        int index = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str.Length <= 3)
+            {
+               NewArray[i] = str;
+               index++;
+            }   
+      }
+       return NewArray;
 }
